460ac4ae23ba49c162dda9f6180e823d5a4cdb2d
Imran Yousuf <imraneecs@gmail.com>
2015-03-04 04:07:49 -0800

    adding new dog works

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,47 @@
 class DogsController < ApplicationController
+	before_action :find_dog, only: [:show, :edit, :update, :destroy]
+
+
+	def index
+	end
+
+	def new
+		@dog=Dog.new
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+
+	def create
+		@dog =Dog.new(dog_params)
+		if @dog.save
+			redirect_to @dog, notice: " You have succesfully added the Dog"
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def dog_params
+		params.require(:dog).permit(:title,:description)
+	end
+
+
+
+	def find_dog
+		@dog = Dog.find(params[:id])
+	end
+
+
 end
